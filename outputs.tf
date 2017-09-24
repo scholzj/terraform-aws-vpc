92495316ae7e547ada5285fb48bfd0c2dff1216a
Jakub Scholz <www@scholzj.com>
2017-09-24 23:10:28 +0200

    Add description to the output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "subnet_ids" {
+    description = "List with IDs of the subnets"
     value = "${aws_subnet.public_subnet.*.id}"
 }
 
 output "vpc_id" {
+    description = "ID of the VPC"
     value = "${aws_vpc.vpc.id}"
 }
